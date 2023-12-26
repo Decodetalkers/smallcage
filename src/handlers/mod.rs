f8e7253ce46c1ea63ce15e63e586017338bf305c
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-12-26 22:27:28 +0800

    chore: handle focused change

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -8,9 +8,9 @@ use crate::SmallCage;
 //
 
 use smithay::input::{SeatHandler, SeatState};
-use smithay::reexports::wayland_server::protocol::wl_surface::WlSurface;
+use smithay::reexports::wayland_server::{protocol::wl_surface::WlSurface, Resource};
 use smithay::wayland::selection::data_device::{
-    ClientDndGrabHandler, DataDeviceHandler, ServerDndGrabHandler,
+    set_data_device_focus, ClientDndGrabHandler, DataDeviceHandler, ServerDndGrabHandler,
 };
 use smithay::wayland::selection::SelectionHandler;
 use smithay::{delegate_data_device, delegate_output, delegate_seat};
@@ -29,7 +29,12 @@ impl SeatHandler for SmallCage {
         _image: smithay::input::pointer::CursorImageStatus,
     ) {
     }
-    fn focus_changed(&mut self, _seat: &smithay::input::Seat<Self>, _focused: Option<&WlSurface>) {}
+
+    fn focus_changed(&mut self, seat: &smithay::input::Seat<Self>, focused: Option<&WlSurface>) {
+        let dh = &self.display_handle;
+        let client = focused.and_then(|s| dh.get_client(s.id()).ok());
+        set_data_device_focus(dh, seat, client)
+    }
 }
 
 delegate_seat!(SmallCage);
